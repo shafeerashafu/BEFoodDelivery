7ccce5328a94df9b9f4a9bce411038434c28708c
shafeerashafu <shafeerazahur3@gmail.com>
2024-06-07 16:43:23 +0530

    added new change

--- index.js
+++ index.js
@@ -9,11 +9,19 @@ const orderRoute = require("./routes/order");
 const app = express();
 
 const cors = require("cors");
+const corsOptions = {
+  origin: 'https://main--fooddeliveryfe.netlify.app', // Your frontend URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // HTTP methods you want to allow
+  allowedHeaders: ['Content-Type', 'Authorization'], // Headers you want to allow
+  credentials: true, // Allow credentials such as cookies
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 dotenv.config();
 const port = process.env.PORT || 8000;
-app.use(cors());
+
 
 app.get("/", (req, res) => {
   res.send("Hello world");
